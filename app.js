ec1de91ac679932ada04571fa2359d92848ab570
Camilo Rueda <camilojrueda@gmail.com>
2023-06-09 00:05:33 -0500

    fix route imports

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/bioRoutes');
+const bioRoutes = require('./routes/bioRoutes');
+const strengthRoutes = require('./routes/strengthRoutes')
+const detailRoutes = require('./routes/detailRoutes')
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(cors());
-app.use('/api', routes); 
+app.use('/api', bioRoutes); 
+app.use('/api', strengthRoutes); 
+app.use('/api', detailRoutes); 
 app.listen(process.env.PORT || 8080, () =>
   console.log(`Server is running`));
